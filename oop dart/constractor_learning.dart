0b81b8a42affd2572d78e37ec28364d65851d3d0
sakib3046 <sakib3046@gmail.com>
2024-01-23 01:57:51 +0600

    i have learned constractor of a class. #sakib3046

--- oop dart/constractor_learning.dart
+++ oop dart/constractor_learning.dart
@@ -1,16 +1,34 @@
+// class Person {
+//   String? name;
+//   int? age;
+
+// //By Named Constractor
+//   Person(String c_name, int c_age) {
+//     print("Hello this is Constractor");
+//     this.name = c_name;
+//     this.age = c_age;
+//   }
+//   void display() {
+//     print("hello ${this.name}. Your age are ${age}");
+//   }
+// }
+
 class Person {
   String? name;
   int? age;
 
-  Person(String c_name, int c_age) {
+//By Named Constractor
+  Person({required c_name, required int c_age}) {
     print("Hello this is Constractor");
     this.name = c_name;
     this.age = c_age;
   }
+  void display() {
+    print("hello ${this.name}. Your age are ${age}");
+  }
 }
 
 void main() {
-  Person sakib = Person("Md Sakib Hasan", 21);
-  print(sakib.name);
-  print(sakib.age);
+  Person sakib = Person(c_name: "Md Rakib Hasan", c_age: 21);
+  sakib.display();
 }
